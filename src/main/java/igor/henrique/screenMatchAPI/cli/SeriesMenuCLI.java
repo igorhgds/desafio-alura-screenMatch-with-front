f1e85b4c97a22e438163e0a37b36a72dc7f2a247
igorhgds <igorhenrique.gomes@hotmail.com>
2025-04-11 10:45:36 -0300

    created display menu

--- src/main/java/igor/henrique/screenMatchAPI/cli/SeriesMenuCLI.java
+++ src/main/java/igor/henrique/screenMatchAPI/cli/SeriesMenuCLI.java
@@ -2,10 +2,32 @@ package igor.henrique.screenMatchAPI.cli;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Scanner;
+
 @Component
 public class SeriesMenuCLI {
-    public void showMenu(){
+    private Scanner scanner = new Scanner(System.in);
 
+    public void showMenu(){
+        var option = -1;
+        while(option != 0){
+            var menu = """
+                    1 - Adicionar Nova Série
+                    2 - Adicionar Episódios
+                    3 - Listar Séries Salvas
+                    4 - Listar Séries Salvas por Categoria
+                    5 - Buscar Série Salva por Título
+                    6 - Filtrar Séries por Avaliação
+                    7 - Top 5 Séries
+                    8 - Top 5 Epsodios por Série
+                    
+                    0 - Sair
+                    """;
+
+            System.out.println(menu);
+            option = scanner.nextInt();
+            scanner.nextLine();
+        }
     }
 }
 
