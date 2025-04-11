ae79b240782cea08712a26fd6292036db6f249a7
igorhgds <igorhenrique.gomes@hotmail.com>
2025-04-11 08:12:28 -0300

    created project base with Spring Initializr

---
+++ screenMatchAPI/src/main/java/igor/henrique/screenMatchAPI/DesafioAluraScreenMatchWithFrontApplication.java
@@ -0,0 +1,13 @@
+package igor.henrique.screenMatchAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioAluraScreenMatchWithFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioAluraScreenMatchWithFrontApplication.class, args);
+	}
+
+}
---
+++ screenMatchAPI/src/test/java/igor/henrique/screenMatchAPI/DesafioAluraScreenMatchWithFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package igor.henrique.screenMatchAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioAluraScreenMatchWithFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
