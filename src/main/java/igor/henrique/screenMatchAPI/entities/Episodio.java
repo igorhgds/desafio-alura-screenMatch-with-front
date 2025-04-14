0ca6a289a1f6b63cecfa7e68c37736b0f7750bfb
igorhgds <igorhenrique.gomes@hotmail.com>
2025-04-14 09:32:53 -0300

    created constructor Episodio

--- src/main/java/igor/henrique/screenMatchAPI/entities/Episodio.java
+++ src/main/java/igor/henrique/screenMatchAPI/entities/Episodio.java
@@ -1,5 +1,6 @@
 package igor.henrique.screenMatchAPI.entities;
 
+import igor.henrique.screenMatchAPI.dtos.episodio.input.GetDataInputDTO;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 @Entity
 @Getter
@@ -32,4 +34,31 @@ public class Episodio {
     @ManyToOne
     private Serie serie;
 
+    public Episodio(Integer seasonNumber, GetDataInputDTO dataEpisodes){
+        this.season = seasonNumber;
+        this.title = dataEpisodes.title();
+        this.episodeNumber = dataEpisodes.episodeNumber();
+
+        try {
+            this.rating = Double.valueOf(dataEpisodes.rating());
+        } catch (NumberFormatException ex) {
+            this.rating = 0.0;
+        }
+
+        try {
+            this.releaseDate = LocalDate.parse(dataEpisodes.releaseDate());
+        } catch (DateTimeParseException ex) {
+            this.releaseDate = null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return
+                " Temporada= " + season +
+                ", titulo= " + title + '\'' +
+                ", numeroEpisodio= " + episodeNumber +
+                ", avaliação= " + rating +
+                ", dataLançamento= " + releaseDate;
+    }
 }
